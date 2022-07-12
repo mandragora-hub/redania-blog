bd8cd987a387ea0306b0387ecc26901fcc6dde60
Royer Antonio Gomez Jimenez <rogomez@hacienda.gov.do>
2022-07-12 16:01:50 -0400

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+// import 'katex/dist/katex.css'
+
+import '@fontsource/inter/variable-full.css'
+
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+
+import siteMetadata from '@/data/siteMetadata'
+// import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
+
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      {isDevelopment && isSocket && <ClientReload />}
+      {/* <Analytics /> */}
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
 }
-
-export default MyApp
