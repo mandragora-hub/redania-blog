d743973e1620082667b396aa83fdc5efb6383a15
mandragora-hub <royer01gomez@gmail.comn>
2022-10-24 15:10:06 +0000

    :dizzy: Add code copy button styles.

--- components/Pre.tsx
+++ components/Pre.tsx
@@ -14,6 +14,8 @@ const Pre = (pre: preProps) => {
     setHovered(true)
   }
   const onExit = () => {
+    if (copied) return
+
     setHovered(false)
     setCopied(false)
   }
@@ -34,7 +36,7 @@ const Pre = (pre: preProps) => {
         ref={textInput}
         onMouseEnter={onEnter}
         onMouseLeave={onExit}
-        className="relative my-4 w-full flex-col rounded border border-gray-300 shadow dark:border-gray-700"
+        className="my-4 w-full flex-col rounded border border-gray-300 shadow dark:border-gray-700"
       >
         {pre.title && (
           <div className="relative flex w-full items-center justify-start rounded-t border-b-2 border-gray-300 bg-gray-50 p-1 dark:border-gray-800 dark:bg-gray-900">
@@ -51,49 +53,30 @@ const Pre = (pre: preProps) => {
             </div>
           </div>
         )}
-
-        {hovered && (
-          <button
-            aria-label="Copy code"
-            type="button"
-            className={`absolute right-2 top-2 h-8 w-8 rounded border-2 bg-gray-700 p-1 dark:bg-gray-800 ${
-              copied
-                ? 'border-green-400 focus:border-green-400 focus:outline-none'
-                : 'border-gray-300'
-            }`}
-            onClick={onCopy}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              fill="none"
-              className={copied ? 'text-green-400' : 'text-gray-300'}
+        {/* Code block content */}
+        <div className="relative">
+          <pre>{pre.children}</pre>
+          <div className="absolute right-2 top-2">
+            <button
+              aria-label="Copy code to clipboard"
+              type="button"
+              title="Copy"
+              className={`flex h-8 w-8 items-center justify-center rounded border ${
+                hovered ? 'opacity-40' : 'opacity-0'
+              } transition-opacity hover:opacity-100 ${copied && 'copy-button-copied'}`}
+              onClick={onCopy}
             >
-              {copied ? (
-                <>
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-                  />
-                </>
-              ) : (
-                <>
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-                  />
-                </>
-              )}
-            </svg>
-          </button>
-        )}
-
-        <pre>{pre.children}</pre>
+              <span className="relative h-4 w-4">
+                <svg viewBox="0 0 24 24" className="copy-button-icon">
+                  <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+                </svg>
+                <svg className="copy-button-success-icon" viewBox="0 0 24 24">
+                  <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
